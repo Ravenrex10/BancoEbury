71ff3e9fd4d0546adece468f46fe2903ad411469
Ravenrex10 <lucas.colbert.eastgate@gmail.com>
2023-03-20 11:53:38 +0100

    Proyecto

---
+++ src/main/java/com/ebury/ebury/EburyApplication.java
@@ -0,0 +1,13 @@
+package com.ebury.ebury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EburyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EburyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ebury/ebury/EburyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebury.ebury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EburyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
