4df4ee194c2e8666fce800ccf853923a7e0153b5
dani-dlg <danieldelizaur@uma.es>
2023-05-07 20:27:08 +0200

    @author en repositories

--- src/main/java/com/ebury/dao/ChatRepository.java
+++ src/main/java/com/ebury/dao/ChatRepository.java
@@ -7,6 +7,9 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
+/**
+ * @author Daniel
+ */
 public interface ChatRepository extends JpaRepository<ChatEntity, Integer> {
 
     @Query("SELECT c FROM ChatEntity c WHERE c.usuarioByClienteA.id = :id OR c.usuarioByClienteB.id = :id")
